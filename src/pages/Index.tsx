a62aec13048a2b028432549b4931980dea20b3bf
arminzaugg <armin.zaugg@gmail.com>
2024-11-29 11:17:42 +0100

    Revert "Fix type error in Index.tsx"
    This reverts commit 081ccfb680a883c9775c8e1e1061be0510e139d6.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -166,7 +166,7 @@ const Index = () => {
           </div>
         </Card>
 
-        <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+        <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen} className="lg:hidden">
           <SheetContent side="bottom" className="h-[80vh] sm:h-[90vh]">
             <div className="h-full pt-6">
               {pdfUrl && <PDFPreview pdfUrl={pdfUrl} />}
